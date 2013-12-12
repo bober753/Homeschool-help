b20f800bd0b2882cd14f35f21307808c05895e8e
Nathan <bober753@gmail.com>
2013-12-12 15:50:51 +0000

    Lesson#1

---
+++ Lesson-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class NamedWelcomeNathan
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the C# Station Tutorial");
+            Console.WriteLine(" ");
+            //Console.ReadKey();
+
+            Console.WriteLine("Hello, {0}!", args[0]);
+            Console.WriteLine("Welcome to the C# Station Tutorial");
+            Console.WriteLine(" ");
+            //Console.ReadKey();
+
+            Console.WriteLine("what is your name?: ");
+            Console.WriteLine("Hello, {0}!", Console.ReadLine());
+            Console.WriteLine("Welcome to the C# Station Tutorial");
+            Console.ReadKey();
+        }
+        
+    }
+}
